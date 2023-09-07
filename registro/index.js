7007750d95b232bc765f78900b791a925cdd6942
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-09-07 10:52:58 -0400

    con otros

--- registro/index.js
+++ registro/index.js
@@ -2,6 +2,8 @@ function cargar() {
     let form = document.querySelector('form')
     form.addEventListener('submit', (event) => {
         event.preventDefault()
+        let data = new FormData(form)
+        
     })
 }
 
